b4f60eb5bf2c7719e63c6b5efe7666d512563d0e
Sindhu Ravuri <sindhuravuri666@gmail.com>
2025-03-22 23:44:41 +0530

    New:BlogModel

---
+++ Models/BlogModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  comments: {
+    type: String,
+    required: true,
+  },
+});
+
+const blogModel = mongoose.model("BlogModel", blogSchema);
+
+export default blogModel;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ConnectDB from "./ConnectDB.js";
-
+import blogModel from "./Models/BlogModel.js";
+import mongoose, { mongo } from "mongoose";
 const app = express();
 //middleware
 app.use(express.json());
@@ -9,6 +10,15 @@ ConnectDB();
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/api/v1/Blog", async (req, res) => {
+  try {
+    const data = await blogModel.find({});
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(8080, () => {
